8c5d143a20a90362b66cd544e0cda400aadd1555
Abdullah-hashmi <abdullah.hashmi24@gmail.com>
2019-09-16 01:54:20 +0530

    last commit

---
+++ app/src/main/java/com/hmi/dealsnxt/Activity/UnverifiedUser.java
@@ -0,0 +1,15 @@
+package com.hmi.dealsnxt.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hmi.dealsnxt.R;
+
+public class UnverifiedUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unverified_user);
+    }
+}
